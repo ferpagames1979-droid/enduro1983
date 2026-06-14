975b2a52e8f10ebd2a470ceb86b1fe1eb79f5e20
fernandopaiva <ferpagames1979@gmail.com>
2026-06-14 10:12:23 -0300

    Aula 1

--- enduro-1983-godot-gd-script/core/controllers/game_view_controller.gd
+++ enduro-1983-godot-gd-script/core/controllers/game_view_controller.gd
@@ -2,3 +2,10 @@ class_name GameViewController
 extends Node2D
 
 const CLASS_NAME_LOG= "GameViewController"
+
+@onready var car_player_view: CarPlayerViewController = %CarPlayerView
+
+func _ready() -> void:
+	PrintLogManager.printlog(CLASS_NAME_LOG, 
+							 PrintLogManager.LogType.INFO,
+							" _ready()")
